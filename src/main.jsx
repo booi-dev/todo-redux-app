41e0e974dee9a342ccdab66d88efd8ec63e214a4
Booi <56810550+booimangang@users.noreply.github.com>
2023-02-28 22:20:18 +0530

    Remove old theme context

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './app/store';
-import ThemeProvider from './context/ThemeContext';
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
+      <App />
     </Provider>
   </React.StrictMode>,
 );
